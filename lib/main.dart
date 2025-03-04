e170cd35bfb4744c24cbf0c26ee2d7ff06b288a1
Mahmoud El-Shahat <114501714+SirMahmoudElshahat@users.noreply.github.com>
2025-03-04 13:01:08 +0200

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:udemy_store/core/app/env.variables.dart';
 import 'package:udemy_store/store_main_widget.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid? await Firebase.initializeApp(
-    options: FirebaseOptions(
-        apiKey: 'AIzaSyDQgcloJ0rNvuLIDAsxMsBwQWxAcT9UdDI',
-        appId: '1:749455263338:android:27aee928934c189a906c13',
-        messagingSenderId: '749455263338',
-        projectId: 'store-95aae')
-  ) : await Firebase.initializeApp() ;
-  runApp(const Store());
-}
 
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: 'AIzaSyDQgcloJ0rNvuLIDAsxMsBwQWxAcT9UdDI',
+            appId: '1:749455263338:android:27aee928934c189a906c13',
+            messagingSenderId: '749455263338',
+            projectId: 'store-95aae',
+          ),
+        )
+      : await Firebase.initializeApp();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) => runApp(const Store()));
+}
